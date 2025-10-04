d1f88dddce2c87b57c1079054679543b7735aaa4
Martin Markmann <martin.markmann@altow.de>
2025-10-04 21:50:51 +0200

    added postgres

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as k8s from "@pulumi/kubernetes";
 import setupNetwork from "./network";
 import setupStorage from "./storage";
 import setupRick from "./srv_rick";
+import { setupPostgress, newDatabase } from "./postgress";
 
 const cfg = new pulumi.Config();
 
@@ -11,3 +12,5 @@ const k8sProvider = new k8s.Provider("local", { kubeconfig: cfg.getSecret("kubec
 setupNetwork(k8sProvider);
 setupStorage(k8sProvider);
 setupRick(k8sProvider);
+setupPostgress(k8sProvider);
+newDatabase("cnpg-test", k8sProvider);
---
+++ src/postgress.ts
@@ -0,0 +1,34 @@
+import * as k8s from "@pulumi/kubernetes";
+
+export function setupPostgress(k8sProvider?: k8s.Provider) {
+
+    const cnpg = new k8s.helm.v3.Release("cnpg", {
+        chart: "cloudnative-pg",
+        repositoryOpts: {
+            repo: "https://cloudnative-pg.github.io/charts/"
+        },
+        namespace: "cnpg-system",
+        createNamespace: true,
+        version: "0.26.0"
+    }, { provider: k8sProvider });
+}
+
+export function newDatabase(namespace: string, k8sProvider?: k8s.Provider) {
+    const cluster = new k8s.helm.v3.Release("cluster", {
+        chart: "cluster",
+        repositoryOpts: {
+            repo: "https://cloudnative-pg.github.io/charts/"
+        },
+        namespace: namespace,
+        createNamespace: true,
+        version: "0.3.1",
+        values: {
+            cluster: {
+                instances: 1,
+                storage: {
+                    storageClass: "local-path"
+                }
+            },
+        }
+    }, { provider: k8sProvider });
+}
