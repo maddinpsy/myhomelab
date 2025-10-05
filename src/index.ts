eccfc68b094e6207e019a1097c95e6faf96b1501
Martin Markmann <martin.markmann@altow.de>
2025-10-05 19:48:30 +0200

    backup cronjob

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 import setupNetwork from "./network";
-import setupStorage from "./storage";
+import { setupStorage, StorageConfig } from "./storage";
 import setupRick from "./srv_rick";
 import { setupPostgress, newDatabase } from "./postgress";
 
@@ -10,7 +10,13 @@ const cfg = new pulumi.Config();
 const k8sProvider = new k8s.Provider("local", { kubeconfig: cfg.getSecret("kubeconfig") });
 
 setupNetwork(k8sProvider);
-setupStorage(k8sProvider);
+
+const storageConfig: StorageConfig = {
+    backupPassword: cfg.requireSecret("backupPassword"),
+    nfsServer: cfg.require("nfsServer"),
+    nfsPath: cfg.require("nfsPath"),
+}
+setupStorage(storageConfig, k8sProvider);
 setupRick(k8sProvider);
 setupPostgress(k8sProvider);
 newDatabase("cnpg-test", k8sProvider);
--- src/storage.ts
+++ src/storage.ts
@@ -1,7 +1,35 @@
 import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
 
+/*
+* Idea behind my storage solution
+I have two device and don't want to buy a new one.
+ - RaspberryPi4
+ - Odroid HC2
+Raspberry runs TalosOS creating a k8s single node cluster
+Odroid runs a NAS Server, there is no TalosOS available
 
-function setupStorage(k8sProvider?: k8s.Provider) {
+Most Application Data is stored on Raspberry PIs SD Card or external USB SSD.
+The data is backed up to NAS Server which runs a 4TB HDD.
+Big Data, like "all Pictures and Videos of all Time" are on the NAS only.
+There is a second 4TB HDD that I will connect once a month to the NAS to manually backup all data to third drive.
+Nothing is encrypted, for now.
+
+In K8S there are:
+- local path provisioner
+- NFS subdir provisioner
+- a Job that syncs the whole SSD content to the NAS once a day using restic
+*/
+
+export type StorageConfig = {
+    backupPassword: pulumi.Output<string>;
+    nfsServer: string;
+    nfsPath: string;
+}
+
+export function setupStorage(storageConfig: StorageConfig, k8sProvider?: k8s.Provider) {
+
+    // Local Path Provisioner
     new k8s.yaml.ConfigFile("provi",
         {
             file: "https://raw.githubusercontent.com/rancher/local-path-provisioner/refs/tags/v0.0.32/deploy/local-path-storage.yaml"
@@ -9,6 +37,45 @@ function setupStorage(k8sProvider?: k8s.Provider) {
         { provider: k8sProvider }
     );
 
+    // Sync CronJob
+    new k8s.batch.v1.CronJob("backup", {
+        metadata: { name: "backup", namespace: "local-path-storage" },
+        spec: {
+            // daily at 2am
+            schedule: "0 2 * * *",
+            jobTemplate: {
+                spec: {
+                    template: {
+                        spec: {
+                            containers: [
+                                {
+                                    name: "restic",
+                                    image: "restic/restic",
+                                    args: [
+                                        "backup", "/local"
+                                    ],
+                                    env: [
+                                        { name: "RESTIC_REPOSITORY", value: "/nfs" },
+                                        { name: "RESTIC_PASSWORD", value: storageConfig.backupPassword },
+                                        { name: "RESTIC_HOST", value: "k8s-node-backup" },
+                                    ],
+                                    volumeMounts: [
+                                        { name: "local", mountPath: "/local" },
+                                        { name: "nfs", mountPath: "/nfs" },
+                                    ],
+                                },
+                            ],
+                            restartPolicy: "OnFailure",
+                            volumes: [
+                                { name: "local", hostPath: { path: "/opt/local-path-provisioner", type: "Directory" } },
+                                { name: "nfs", nfs: { server: storageConfig.nfsServer, path: storageConfig.nfsPath } },
+                            ],
+                        },
+                    },
+                },
+            },
+        },
+    }, { provider: k8sProvider });
+
 }
 
-export default setupStorage;
