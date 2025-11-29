109d60c6836f7a192f747756110cad82a1ff91c6
Martin Markmann <martin.markmann@altow.de>
2025-11-30 00:28:05 +0100

    remove test services

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 import setupNetwork from "./network";
 import { setupStorage, StorageConfig } from "./storage";
-import setupRick from "./srv_rick";
 import { setupPostgres, newDatabase } from "./postgres";
 import { setupMinio } from "./minio";
 import { createRegistry } from "./registry";
@@ -22,11 +21,7 @@ setupStorage(storageConfig, k8sProvider);
 
 let network = setupNetwork(k8sProvider);
 
-setupRick(k8sProvider, network);
-
 let postgres = setupPostgres(k8sProvider);
-let ns = new k8s.core.v1.Namespace("cnpg-test", { metadata: { name: "cnpg-test" } }, { provider: k8sProvider });
-newDatabase("cnpg-test", ns, k8sProvider, postgres);
 
 let minio = setupMinio(k8sProvider, network);
 
