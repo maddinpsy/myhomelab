5378a1dafa599051cb8334c7ec47324899330bfa
maddinpsy <maddinpsycho@gmail.com>
2026-02-01 20:07:48 +0000

    something

--- docker_gitops.sh
+++ docker_gitops.sh
@@ -3,7 +3,6 @@
 # pull current repo 
 echo checking for updates...
 git fetch
-echo current commit: $(git rev-parse HEAD)
 if ! git diff origin/docker docker --exit-code 
 then
     echo changes detected, updating...
