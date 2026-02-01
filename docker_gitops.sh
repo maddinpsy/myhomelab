4be421e7f61cd1733112ec1c088eb27b65f642ed
maddinpsy <maddinpsycho@gmail.com>
2026-02-01 19:34:53 +0000

    first run on gitops

---
+++ docker_gitops.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# pull current repo
+git pull
+
+# check corn job already exists or add it
+(crontab -l | grep -q 'docker_gitops.sh') || (crontab -l ; echo "*/5 * * * * /bin/bash $(pwd)/docker_gitops.sh >> $(pwd)/docker_gitops.log 2>&1") | crontab -
+
+# add docker packge repo if not exists
+if ! grep -q "^deb .\+download.docker.com" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
+
+# update apt and install docker
+sudo apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# login to tailscale
+sudo tailscale up
+
+# run docker compose
+docker compose -f docker-compose.yml up -d --remove-orphans
